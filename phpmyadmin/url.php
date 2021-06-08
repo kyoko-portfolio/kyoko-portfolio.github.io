d6793f0f29a3d783fdb3d9c5827612a5fef5d6d3
EikoT <eikot01@gmail.com>
2021-06-07 21:54:23 -0700

    Landing Page
    First proper commit

---
+++ phpmyadmin/url.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://eikot.github.io/phpmyadmin/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://eikot.github.io/phpmyadmin/";
+		</script>
+
+		<p>You are being redirected to <a href="https://eikot.github.io/phpmyadmin/">https://eikot.github.io/phpmyadmin/</a></p>
+	</body>
+</html>
